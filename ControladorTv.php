07dcdb7a119cccf4d8d4807a239f21544fc6b32f
Jamil Zabotto Neto <jamilzabneto@hotmail.com>
2017-01-07 18:37:00 -0200

    Create ControladorTv.php

---
+++ ControladorTv.php
@@ -0,0 +1,13 @@
+<?php
+
+
+interface ControladorTv {
+    public function ligar();
+    public function desligar();
+    public function abrirMenu();
+    public function fecharMenu();
+    public function maisVolume();
+    public function menosVolume();
+    public function maisCanal();
+    public function menosCanal();
+}
