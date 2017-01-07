483e51b9137976a0204594fff9f740ab15adf4a2
Jamil Zabotto Neto <jamilzabneto@hotmail.com>
2017-01-07 18:36:35 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt=br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Controle de Televisão</h1>
+        <?php
+            require_once 'ControleTv.php';
+            $c = new ControleTv();
+            //$c->ligar();  
+            //$c->maisCanal();
+            //$c->maisVolume();
+           //$c->desligar();            
+            //$c->menosCanal();
+            //$c->menosVolume();
+            //$c->abrirMenu();
+            //$c->fecharMenu();
+        ?>
+    </body>
+</html>
